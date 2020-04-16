2ae578e147091201d9b4948d00b1fc19a9aba848
JackOra <18567032383@163.com>
2020-04-16 23:07:51 +0800

    Three Commit

---
+++ python打印三角形和正方形/sanjiao.py
@@ -0,0 +1,5 @@
+n = int(input('请输入一个整数:'))
+for i in range(n):
+    print(" "*(n-i) + "*"*(2*(i+1)-1)
+
+
---
+++ python打印三角形和正方形/square.py
@@ -0,0 +1,11 @@
+n = int(input("请输入一个整数:"))
+for i in range(1,n+1):
+    for j in range(1.n+1):
+        if i < j:
+            print(str(j)+" ",end="")
+        else:
+            print('1'+ " ",end = "")
+    print(" ",end = '\n')
+
+
+#此题注重的是打印符号的使用
